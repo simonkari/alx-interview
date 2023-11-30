c32e00877bd26bb52956d64cd1973212491b1f62
simonkari <kariithisimon1715@gmail.com>
2023-11-30 22:43:48 +0300

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-Creates a function that returns a list of lists of
-integers representing Pascal's triangle of n.
+""" 
+Creates a function that returns a lits of lists
+of integers representing the Pascal's triangle of n 
 """
 
 
 def pascal_triangle(n):
-    """
-    Generates Pascal's triangle up to n rows.
-
-    Args:
-        n (int): Number of rows for Pascal's triangle.
-
-    Returns:
-        list of lists: Pascal's triangle up to n rows.
-    """
+    """ Generates Pascal's triangle up to n rows.
+    n:
+        number of rows
+    return:
+        list of lists: Pascal's triangle up to n rows."""
     new_pascal = []
 
-    # Assumes that n is a non-negative integer
+    """ Assumes that n is a non-negative integer """
     if n <= 0:
         return new_pascal
 
@@ -25,14 +21,8 @@ def pascal_triangle(n):
         row_index = [1]
         if new_pascal:
             final_row = new_pascal[-1]
-            row_index.extend([sum(pair) for pair in zip(final_row, final_row[1:])])
+            row_index.extend([sum(pair) for pair in
+                              zip(final_row, final_row[1:])])
             row_index.append(1)
         new_pascal.append(row_index)
-    return new_pascal
-
-
-# Example usage:
-# Uncomment the following lines to test the function with a specific value of n
-# n_value = 5
-# result = pascal_triangle(n_value)
-# print(result)
+    return (new_pascal)
