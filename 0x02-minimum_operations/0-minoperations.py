cb1370166aeacc0135556d17921b3ae5834666b4
simonkari <kariithisimon1715@gmail.com>
2023-12-14 19:17:08 +0300

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 """
-This script defines a method for performing operations on a single
-character 'H' with two operations: Copy All and Paste.
-Given a number 'n', the goal is to calculate the minimum number of
-operations needed to result in exactly 'n' occurrences of 'H'.
+This script defines a method for performing operations on a
+single character 'H' with two operations: Copy All and Paste.
+Given a number 'n', the goal is to calculate the minimum
+number of operations needed to result in exactly 'n'
+occurrences of 'H'.
 
 Module Docstring:
 -----------------
@@ -16,40 +17,21 @@ Return an integer
 if n is impossible to achieve, return 0
 """
 
+
 def minOperations(n):
     """
     Single character H
     Fewest number of operations
-
-    Parameters:
-    -----------
-    n : int
-        The target number of occurrences of 'H'.
-
-    Returns:
-    --------
-    int
-        The minimum number of operations required to achieve
-        'n' occurrences of 'H'.
-        Returns 0 if it is impossible to achieve 'n'.
     """
 
-    # Base case: If n is less than or equal to 1, no 
-    #operations are needed.
     if n <= 1:
         return 0
-
-    # Initialize variables
     numbr, index, operations = n, 2, 0
 
-    # Iterate until numbr is reduced to 1
     while numbr > 1:
-        # Check if numbr is divisible by index
         if numbr % index == 0:
             numbr = numbr / index
             operations = operations + index
         else:
-            # If not divisible, increment index
             index += 1
-
     return operations
